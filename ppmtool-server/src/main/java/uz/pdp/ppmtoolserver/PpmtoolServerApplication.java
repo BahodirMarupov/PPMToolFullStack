d46cc709073a21cf2a39dc957660827aba103401
Liam <bahodirmusayevich@gmail.com>
2020-07-22 17:58:51 +0500

    version 1

---
+++ ppmtool-server/src/main/java/uz/pdp/ppmtoolserver/PpmtoolServerApplication.java
@@ -0,0 +1,13 @@
+package uz.pdp.ppmtoolserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpmtoolServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PpmtoolServerApplication.class, args);
+    }
+
+}
---
+++ ppmtool-server/src/test/java/uz/pdp/ppmtoolserver/PpmtoolServerApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.pdp.ppmtoolserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpmtoolServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
